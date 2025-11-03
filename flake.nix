c0387881fd66e83de15520cd77ccaa10b3ae4637
Quin <lamanocordero@gmail.com>
2025-11-03 21:34:03 +0800

    pls

--- flake.nix
+++ flake.nix
@@ -1,45 +1,47 @@
 {
   description = "niri";
+
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-unstable";
+
     home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+
     quickshell = {
       url = "github:outfoxxed/quickshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.quickshell.follows = "quickshell";  # Use same quickshell version
+      inputs.quickshell.follows = "quickshell";
     };
   };
 
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
-  nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-	system = "x86_64-linux";	 	
-  specialArgs = { inherit inputs; };
+      specialArgs = { inherit inputs; };
+
       modules = [
         ./configuration.nix
         ./noctalia.nix
-        home-manager.nixosModules.home-manager{
-            home-manager = {
-                useGlobalPkgs = true;
-                useUserPackages = true;
-                users.quin = import ./home.nix;
-            };
+
+        # Add Home Manager as a module
+        home-manager.nixosModules.home-manager
+        {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.quin = import ./home.nix;
+          };
         }
       ];
+      
     };
   };
 }
-
-
-
-
-
-
-
