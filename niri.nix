bbf91788b74905c52c19faa4ed4ebb98ceaa7c2a
Quin <lamanocordero@gmail.com>
2025-11-03 21:30:08 +0800

    pls

--- niri.nix
+++ niri.nix
@@ -1,24 +1,43 @@
-{ config, lib, pkgs, ... }:
+{ pkgs, inputs, ... }:
+
+let
+  # Helper function for noctalia-shell IPC calls
+  noctalia = cmd: [
+    "noctalia-shell" "ipc" "call"
+  ] ++ (pkgs.lib.splitString " " cmd);
+in
 {
-    xdg.configFile."niri/config.kdl".source = ./config/niri/config.kdl;
-    # home.file.".config/waybar".source = ./config/waybar;
-    home.file.".config/rofi".source = ./config/rofi;
-    xdg.configFile."niri/config.kdl".force = true;
-    # programs.waybar.enable = true;
-    
-    home.packages = [ 
-        pkgs.xwayland-satellite 
-        pkgs.xwayland-run 
-        pkgs.cage
-        pkgs.squeekboard 
-        pkgs.brightnessctl
-        pkgs.swaybg
-        pkgs.swaylock
-        pkgs.wl-clipboard 
-        pkgs.rofi
-        pkgs.fuzzel
+#  home.file.".config/rofi".source = ./config/rofi;
+
+  home-manager.users.quin = { config, lib, pkgs, ... }: {
+    home.packages = [
+      pkgs.xwayland-satellite
+      pkgs.xwayland-run
+      pkgs.cage
+      pkgs.squeekboard
+      pkgs.brightnessctl
+      pkgs.swaybg
+      pkgs.swaylock
+      pkgs.wl-clipboard
+      pkgs.rofi
+      pkgs.fuzzel
     ];
 
+    programs = {
+      niri = {
+        enable = true;
+        settings = {
+          binds = {
+            # --- Noctalia integration keybinds ---
+            "Mod+Space" = { action.spawn = noctalia "launcher toggle"; };
+            "Mod+P"     = { action.spawn = noctalia "sessionMenu toggle"; };
+            "Mod+L"     = { action.spawn = noctalia "lockScreen toggle"; };
+            "XF86AudioLowerVolume" = { action.spawn = noctalia "volume decrease"; };
+            "XF86AudioRaiseVolume" = { action.spawn = noctalia "volume increase"; };
+            "XF86AudioMute"        = { action.spawn = noctalia "volume muteOutput"; };
+          };
+        };
+      };
+    };
+  };
 }
-
-
