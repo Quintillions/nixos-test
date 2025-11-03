9c5667be7bb4af16466a4c67b88d8389bf707250
Quintillions <lamanocordero@gmail.com>
2025-11-03 14:31:02 +0800

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -6,22 +6,6 @@
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
     };
-    dgop = {
-      url = "github:AvengeMedia/dgop";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dms-cli = {
-      url = "github:AvengeMedia/danklinux";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    dankMaterialShell = {
-      url = "github:AvengeMedia/DankMaterialShell";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.dgop.follows = "dgop";
-      inputs.dms-cli.follows = "dms-cli";
-    };
   };
 
   outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
@@ -40,6 +24,7 @@
     };
   };
 }
+
 
 
 
