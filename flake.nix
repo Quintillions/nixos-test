154ce3ab8c6df8dc8b29fb783363f3096c2a4451
Quin <lamanocordero@gmail.com>
2025-11-03 20:15:24 +0800

    a

--- flake.nix
+++ flake.nix
@@ -6,13 +6,24 @@
         url = "github:nix-community/home-manager";
         inputs.nixpkgs.follows = "nixpkgs";
     };
+    quickshell = {
+      url = "github:outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    noctalia = {
+      url = "github:noctalia-dev/noctalia-shell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.quickshell.follows = "quickshell";  # Use same quickshell version
+    };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, ...}: {
+
+  outputs = inputs@{ self, nixpkgs, ...}: {
   nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
 	system = "x86_64-linux";	 	
       modules = [
         ./configuration.nix
+        ./noctalia.nix
         home-manager.nixosModules.home-manager{
             home-manager = {
                 useGlobalPkgs = true;
