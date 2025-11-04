4be69ba992eed232f1ff4de22445c7ae5a31ed1a
Quin <lamanocordero@gmail.com>
2025-11-04 10:44:59 +0800

    34

--- niri.nix
+++ niri.nix
@@ -17,8 +17,8 @@ in
       binds = with config.lib.niri.actions; {
         "Mod+L".action.spawn = noctalia "lockScreen lock";
         "Mod+D".action.spawn = noctalia "launcher toggle";
-        "Mod+E".action.spawn = noctalia "nautilus";
-        "Mod+T".action.spawn = noctalia "alacritty";
+        "Mod+E".action.spawn = "nautilus";
+        "Mod+T".action.spawn = "alacritty";
         "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
         "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
         "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
