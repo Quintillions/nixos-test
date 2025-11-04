0046b7cee1b419790c6fdba0952e197af7742e70
Quin <lamanocordero@gmail.com>
2025-11-04 10:56:03 +0800

    2131231223

--- niri.nix
+++ niri.nix
@@ -15,7 +15,7 @@ in
         }
       ];
       binds = with config.lib.niri.actions; {
-        "Mod+Ctrl+L".action.spawn = noctalia "lockScreen lock";
+        "Mod+P".action.spawn = noctalia "lockScreen lock";
         "Mod+D".action.spawn = noctalia "launcher toggle";
         "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
         "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
