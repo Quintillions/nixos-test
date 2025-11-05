7a7bc11cabfe583faebee2175f8d1a0d0947ac85
Quin <lamanocordero@gmail.com>
2025-11-05 10:46:21 +0800

    fix typo

--- alacritty.nix
+++ alacritty.nix
@@ -2,7 +2,7 @@
 {
   programs.zsh = {
     enable = true;
-    autosuggestion.emable = true;
+    autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     oh-my-zsh = {
       enable = true;
