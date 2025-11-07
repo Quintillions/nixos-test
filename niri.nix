998a2e49f0694575b3a80418fdbd6a989251b590
Quintillions <lamanocordero@gmail.com>
2025-11-07 21:49:59 +0800

    Update niri.nix

--- niri.nix
+++ niri.nix
@@ -68,6 +68,7 @@ in
         "XF86AudioLowerVolume".action.spawn = noctalia "volume decrease";
         "XF86AudioRaiseVolume".action.spawn = noctalia "volume increase";
         "XF86AudioMute".action.spawn = noctalia "volume muteOutput";
+        "XF86AudioMicMute".action.spawn = noctalia "volume muteInput";
         "Mod+E".action.spawn = "nautilus";
         "Mod+Return".action.spawn = "alacritty";
           # Overview and overlay
@@ -177,3 +178,4 @@ in
     };
   };
 }
+
